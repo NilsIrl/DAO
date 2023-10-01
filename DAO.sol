0bb44d2bf38c2ac088100362a08e3d5a854f127c
Nils <nils@nilsand.re>
2023-10-01 03:01:16 -0700

    Add exploit

--- DAO.sol
+++ DAO.sol
@@ -1,12 +1,16 @@
-// The DAO contract itself
 contract DAO is DAOInterface, Token, TokenCreation {
     /* ... */
     function getMyReward() noEther returns (bool _success) {
+        // msg.sender is the caller. An instance of "Exploit" 
         return withdrawRewardFor(msg.sender);
     }
 
-
     function withdrawRewardFor(address _account) noEther internal returns (bool _success) {
+        // Assume that initially
+        // balanceOf(_account) == 100
+        // paidOut[_account] == 0
+        // rewardAccount.accumulatedInput() == 100
+        // totalSupply == 200
         if ((balanceOf(_account) * rewardAccount.accumulatedInput()) / totalSupply < paidOut[_account])
             throw;
 
--- ManagedAccount.sol
+++ ManagedAccount.sol
@@ -1,9 +1,13 @@
 contract ManagedAccount is ManagedAccountInterface{
     /* ... */
     function payOut(address _recipient, uint _amount) returns (bool) {
+        // Here msg.sender == address of caller, the "DAO" contract
+        // msg.value == 0 && payOwnerOnly == false
         if (msg.sender != owner || msg.value > 0 || (payOwnerOnly && _recipient != owner))
             throw;
         if (_recipient.call.value(_amount)()) {
+            // PayOut is a predefined event that can be listened to. This is
+            // not relevant to the challenge.
             PayOut(_recipient, _amount);
             return true;
         } else {
